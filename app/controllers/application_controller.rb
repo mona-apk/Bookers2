66201087958140d92c7e2707e93784fd957c3557
mona-apk <cordion_nap@yahoo.co.jp>
2020-05-17 00:25:02 +0900

    feat: ログイン後のリダイレクト先の変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,18 @@ class ApplicationController < ActionController::Base
 	before_action :authenticate_user!
 	before_action :configure_permitted_parameters, if: :devise_controller?
 
+	#sign in後のリダイレクト
+	def after_sign_in_path_for(resource)
+		#TODO: パスの変更
+		new_book_path
+	end
+
+	#sign out後のリダイレクト
+	def after_sign_out_path_for(resource)
+		#TODO: パスの変更
+		new_book_path
+	end
+
 	protected
 
 	def configure_permitted_parameters

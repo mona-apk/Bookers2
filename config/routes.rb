96c32709a1a8516555b3ca461a006bb919230224
mona-apk <cordion_nap@yahoo.co.jp>
2020-05-17 09:25:37 +0900

    feat: Userのプロフィール画像を変更可能に

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   resources :books
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :update]
 end
+
+Refile.secret_key = '8da485e1f1ea7c5fb9c0a6a542a71420e6d609a1d21df906cd85efb2337f028d02a5e2d9ad37e622b52f1df9e9b278e48c909058de26281c46df310f5ad070a1'
